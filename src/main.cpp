0af296b6b315eefeeaa81336d242fa46295ec1ec
daipoyo <mahogani44@gmail.com>
2021-01-10 01:02:51 +0900

    20210110

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,36 @@
 #include <string.h>
 #include <M5EPD.h>
 #include <WiFi.h> 
+#include "FS.h"
+#include "SPIFFS.h"
 #include "tinyxml2.h"
 
 M5EPD_Canvas canvas(&M5.EPD);
 using namespace tinyxml2;
 
 void setup() {
-  
-  Serial.begin(115200);
+
   M5.begin();
+  M5.TP.SetRotation(90);
+  M5.EPD.SetRotation(90);
+  M5.EPD.Clear(true);
+
+  canvas.loadFont("/ipaexg.ttf", SD); 
   
-  WiFi.begin("WIFI-SSID", "WIFI-PASSWORD"); 
+  WiFi.begin("********", "********"); 
   while (WiFi.status() != WL_CONNECTED) { 
-      delay(500); 
+      delay(100); 
       Serial.print("."); 
   }
+
+  Serial.println();
+  Serial.print("WiFi connected: ");
+  Serial.println(WiFi.localIP());
+
+  // if (!SPIFFS.begin(true)){
+  //   log_e("SPIFFS Mount Failed");
+  //   while(1);
+  // }
 
   //XMLNode * root = xmlDocument.FirstChild();
   //XMLElement * element = root->FirstChildElement("element");
@@ -30,39 +45,51 @@ void setup() {
 
 void loop() {
   
+  M5.begin();  
   M5.EPD.Clear(true);
+
+  // if(SD.exists("/ipaexg.ttf")) { 
+  //   canvas.loadFont("/ipaexg", SD); 
+  // }
+
   canvas.createCanvas(540, 960);
-  canvas.setTextSize(3);
+  //canvas.setTextSize(32);
+  canvas.createRender(42, 256);
+  canvas.setTextSize(42);
+  canvas.setTextColor(15);
+  canvas.drawString("ブッシープレコの世界", 45, 450);
+  canvas.pushCanvas(0,0,UPDATE_MODE_DU4);
 
-  XMLDocument doc;
-  doc.LoadFile( "https://www.nhk.or.jp/rss/news/cat0.xml" );
-  doc.Print();
-/*
-  if(doc.Parse(xmlData)!= XML_SUCCESS){
-    Serial.println("Error parsing");  
-    return;
-  };
-*/
-  XMLNode * channel = doc.FirstChild();
-  XMLElement * top_title = channel->FirstChildElement("title");
-  std::string top_midashi = top_title->GetText();
+//   XMLDocument doc;
+//   doc.LoadFile( "https://www.nhk.or.jp/rss/news/cat0.xml" );
+//   doc.Print();
+// /*
+//   if(doc.Parse(xmlData)!= XML_SUCCESS){
+//     Serial.println("Error parsing");  
+//     return;
+//   };
+// */
+//   XMLNode * channel = doc.FirstChild();
+//   XMLElement * top_title = channel->FirstChildElement("title");
+//   std::string top_midashi = top_title->GetText();
 
-  //XMLElement * item = channel.FirstChildElement("item");
-  int pos_x = 50;
-  int pos_y = 50;
-  for(XMLElement * item = channel->FirstChildElement("item"); item!= NULL; item = item->NextSiblingElement("item")) {
+//   //XMLElement * item = channel.FirstChildElement("item");
+//   int pos_x = 50;
+//   int pos_y = 50;
+//   for(XMLElement * item = channel->FirstChildElement("item"); item!= NULL; item = item->NextSiblingElement("item")) {
     
-    XMLElement * title = item->FirstChildElement("title");
-    XMLElement * description = title->FirstChildElement("description");
+//     XMLElement * title = item->FirstChildElement("title");
+//     XMLElement * description = title->FirstChildElement("description");
     
-    canvas.drawString(title->GetText(), pos_x, pos_y);  
-    canvas.drawString(description->GetText(), pos_x, pos_y + 10);  
-    pos_y = pos_y + 20;
+//     canvas.drawString(title->GetText(), pos_x, pos_y);  
+//     canvas.drawString(description->GetText(), pos_x, pos_y + 10);
+//     canvas.pushCanvas(0,0,UPDATE_MODE_DU4); 
+//     pos_y = pos_y + 20;
 
     //std::string midashi = title->GetText();
     //std::string naiyou = description->GetText();  
-  }
+  // }
 
-  sleep(3600);
+  sleep(600);
 
 }
